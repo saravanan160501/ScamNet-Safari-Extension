802f7ff2beb0962929d940742982d1c829ecb5a8
Mohan Saravanan <saravanan160501@outlook.com>
2024-02-12 06:25:58 +0800

    Removed internal link logic

--- content.js
+++ content.js
@@ -8,15 +8,6 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     } else if (request.action === "others") {
         if (window.location.protocol === "http:") {
             browser.runtime.sendMessage({ action: "Likely Scam", host: host, reason: "Browse with caution!\nThis website is not using the https protocol.\n\nStay vigilant and check with official sources if you are about to do sensitive transactions on this website such as entering login credentials, OTP, payment information, particulars and etc."});
-        } else if (!Array.from(document.querySelectorAll('a')).some(link => {
-            try {
-                const linkURL = new URL(link.href);
-                return linkURL.hostname === host;
-            } catch (error) {
-                return false;
-            }
-        })) {
-            browser.runtime.sendMessage({ action: "Likely Scam", host: host, reason: "Browse with caution!\nThe website's structure is suspicious.\n\nStay vigilant and check with official sources if you are about to do sensitive transactions on this website such as entering login credentials, OTP, payment information, particulars and etc."});
         }
     }
 });
